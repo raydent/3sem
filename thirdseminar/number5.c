66251c760ea4aa4333336aead78ce03a4d53490b
raydent <benisgachi@gmail.com>
2019-10-03 01:24:08 +0300

    1

--- thirdseminar/number5.c
+++ thirdseminar/number5.c
@@ -5,13 +5,18 @@
 #include <sys/wait.h>
 #include <string.h>
 const int N = 100;
+
 typedef struct mystruct {
     char* commandline;
     char** args;
     int killtime;
-}container;
+    int argnum;
+}container_t;
+
 int getargnum(char* string);
 char** makeargarr(char* string);
+container_t* fillcontainer(int stringnum , FILE* f);
+
 int main(){
     int stringnum = 0;
     FILE* f = fopen("source.txt", "r");
@@ -20,33 +25,29 @@ int main(){
     fgets(buf, N, f);
     //stringnum = (buf[0] - 9);
     printf("stringnum = %d\n", stringnum);
-    char** stringarr = (char**) calloc (stringnum, sizeof(char*));
-    for(int i = 0; i < stringnum; i++){
-        stringarr[i] = (char*) calloc (N, sizeof(char));
-    }
-    for(int i = 0; i < stringnum; i++){
-        fgets(stringarr[i], N, f);
-        printf("%s", stringarr[i]);
-    }
+    container_t* container = fillcontainer(stringnum , f);
     int frk = 1;
     printf("0\n");
     for(int i = 0; i < stringnum - 1; i++){
         printf("1\n");
         frk = fork();
         if (frk == 0){
-            printf("we're here\n");
+            printf("we're here 1\n");
             //int argnum = getstringnum(stringarr[i]);
-            char** argarr = makeargarr(stringarr[i]);
-            execvp(argarr[1], argarr + 1);
+
+            execvp(container[i].args[1], container[i].args + 1);
             //free(argarr);
         }
     }
     printf("1\n");
+    for(int i = 0; i <= container[stringnum - 1].argnum; i++){
+        printf("%d string is %s\n", i, container[stringnum - 1].args[i]);
+    }
     if (frk > 0){
-        printf("we're here");
+        printf("we're here 2");
         //wait(NULL);
-        char** argarr = makeargarr(stringarr[stringnum - 1]);
-        execvp(argarr[1], argarr + 1);
+        //char** argarr = makeargarr(stringarr[stringnum - 1]);
+        execvp(container[stringnum - 1].args[1], container[stringnum - 1].args + 1);
         //free(argarr);
     }
     fclose(f);
@@ -54,14 +55,17 @@ int main(){
 int getargnum(char* string){
     char* space = string;
     int n = 0;
-    while(space != NULL){
-        space = strstr(space, " ");
+    while(space != NULL) {
+        //printf("%d\n", n);
+        space = strstr(space + 1, " ");
+        n++;
     }
     return n;
 }
 char** makeargarr(char* string){
-    printf("i'm dying\n");
+    //printf("i'm dying\n");
     int argnum = getargnum(string);
+    //printf("argnum = %d", argnum);
     char** argarr = (char**) calloc (argnum + 1, sizeof(char*));
     char *p1, *p2;
     p1 = string;
@@ -69,14 +73,35 @@ char** makeargarr(char* string){
     int i = 0;
     while(p2 != NULL){
         p2 = strstr(p2, " ");
-        if(p2 == NULL)
+        if (p2 == NULL && p1 != NULL){
+            printf("i = %d, p1 = %s\n", i, p1);
+            argarr[i] = (char*) calloc (strlen(string) - (p1 - string), sizeof(char));
+            strcpy(argarr[i], p1);
             break;
+        }
         argarr[i] = (char*) calloc (p2 - p1, sizeof(char));
         strncpy(argarr[i], p1, p2 - p1);
+        printf("s = %s %s", p1, p2);
+        p2++;
+        if(p2 == NULL)
+            break;
         p1 = p2;
         i++;
     }
-    argarr[i] = NULL;
-    printf("now we are dying here");
+    argarr[i + 1] = NULL;
+    //printf("now we are dying here");
     return argarr;
+}
+container_t* fillcontainer(int stringnum , FILE* f){
+    container_t* container = (container_t*) calloc (stringnum, sizeof(container_t));
+    for(int i = 0; i < stringnum; i++){
+        container[i].commandline = (char*) calloc (N, sizeof(char));
+        fgets(container[i].commandline, N, f);
+        container[i].args = makeargarr(container[i].commandline);
+        container[i].argnum = getargnum(container[i].commandline);
+        container[i].killtime = (int)(container[i].commandline[0] - '0');
+        //printf("string = %s", container[i].commandline);
+        //printf("argnum = %d, killtime = %d\n", container[i].argnum, container[i].killtime);
+    }
+    return container;
 }
