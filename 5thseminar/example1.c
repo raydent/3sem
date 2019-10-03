afb6bbe7af2eca4fc80c80e6d16465fe56227135
raydent <benisgachi@gmail.com>
2019-10-03 11:46:29 +0300

    now using functions

--- 5thseminar/example1.c
+++ 5thseminar/example1.c
@@ -8,52 +8,32 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+void chatting(char* nametoread, char* nametowrite, int fd);
 
 int main(int argc, char** argv){
-int fd, result;
-size_t size;
-char resstring[14];
-char name01[]="aaaa.fifo";
-char name10[] = "aaa.fifo";
-(void)umask(0);
-
-if(mknod(name01, S_IFIFO | 0666, 0) < 0);
-int hh = mknod(name10, S_IFIFO | 0666, 0);
-printf("hh = %d\n", hh);
-
-if (strcmp(argv[1], "0") == 0){
-    printf("0\n");
-        int frk = fork();
-    while(1){
-        if (frk == 0){
-            fd = open(name01, O_WRONLY);
-            char buf[100];
-            memset(buf, 0, 100);
-            fgets(buf, 100, stdin);
-            write(fd, buf, 100);
-            memset(buf, 0, 100);
-            close(fd);
-        }
-        else{
-
-            fd = open(name10, O_RDONLY);
-            char buf[100];
-            memset(buf, 0, 100);
-            read(fd, buf, 100);
-            printf("%s", buf);
-            memset(buf, 0, 100);
-            close(fd);
-        }
+    int fd, result;
+    size_t size;
+    char resstring[14];
+    char name01[]="aaaa.fifo";
+    char name10[] = "aaa.fifo";
+    (void)umask(0);
+    if(mknod(name01, S_IFIFO | 0666, 0) < 0);
+    int hh = mknod(name10, S_IFIFO | 0666, 0);
+    printf("hh = %d\n", hh);
+    if (strcmp(argv[1], "0") == 0){
+        chatting(name10, name01, fd);
     }
-    kill(getpid(), SIGKILL);
+    if (strcmp(argv[1], "1") == 0){
+        chatting(name01, name10, fd);
+    }
+    return 0;
 }
-if (strcmp(argv[1], "1") == 0){
-    printf("1\n");
+void chatting(char* nametoread, char* nametowrite, int fd){
     int frk = fork();
     while(1){
         int frk = fork();
         if (frk == 0){
-            fd = open(name10, O_WRONLY);
+            fd = open(nametowrite, O_WRONLY);
             char buf[100];
             memset(buf, 0, 100);
             fgets(buf, 100, stdin);
@@ -62,7 +42,7 @@ if (strcmp(argv[1], "1") == 0){
             close(fd);
         }
         else{
-            fd = open(name01, O_RDONLY);
+            fd = open(nametoread, O_RDONLY);
             char buf[100];
             memset(buf, 0, 100);
             read(fd, buf, 100);
@@ -71,6 +51,4 @@ if (strcmp(argv[1], "1") == 0){
             close(fd);
         }
     }
-}
-return 0;
 }
