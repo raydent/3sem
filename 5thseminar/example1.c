5cb75606669c93f7a19e069bc4eff0f97c20bc3b
raydent <benisgachi@gmail.com>
2019-10-10 11:45:43 +0300

    now using threads

--- 5thseminar/example1.c
+++ 5thseminar/example1.c
@@ -7,7 +7,36 @@
 #include <time.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <pthread.h>
 
+void *mythread1(void *names){
+    pthread_t mythid;
+    mythid = pthread_self();
+    int fdw = open((char*)names, O_WRONLY);
+    while(1){
+        char buf[100];
+        memset(buf, 0, 100);
+        fgets(buf, 100, stdin);
+        write(fdw, buf, 100);
+        memset(buf, 0, 100);
+    }
+    //printf("Thread %lu, Calculation result = %d\n", mythid, a);
+    return NULL;
+}
+void *mythread2(void *names){
+    pthread_t mythid;
+    mythid = pthread_self();
+    int fwr = open((char*)names, O_RDONLY);
+    while(1){
+       char buf[100];
+       memset(buf, 0, 100);
+       read(fwr, buf, 100);
+       printf("%s", buf);
+       memset(buf, 0, 100);
+    }
+    //printf("Thread %lu, Calculation result = %d\n", mythid, a);
+    return NULL;
+}
 void chatting(char* nametoread, char* nametowrite, int fd);
 
 int main(int argc, char** argv){
@@ -15,7 +44,7 @@ int main(int argc, char** argv){
     size_t size;
     char resstring[14];
     char name01[]="aaaa.fifo";
-    char name10[] = "aaa.fifo";
+    char name10[] = "aaa.fifo"; 
     (void)umask(0);
     mknod(name01, S_IFIFO | 0666, 0);
     mknod(name10, S_IFIFO | 0666, 0);
@@ -29,26 +58,13 @@ int main(int argc, char** argv){
 }
 
 void chatting(char* nametoread, char* nametowrite, int fd){
-    int frk = fork();
-    while(1){
-        int frk = fork();
-        if (frk == 0){
-            fd = open(nametowrite, O_WRONLY);
-            char buf[100];
-            memset(buf, 0, 100);
-            fgets(buf, 100, stdin);
-            write(fd, buf, 100);
-            memset(buf, 0, 100);
-            close(fd);
-        }
-        else{
-            fd = open(nametoread, O_RDONLY);
-            char buf[100];
-            memset(buf, 0, 100);
-            read(fd, buf, 100);
-            printf("%s", buf);
-            memset(buf, 0, 100);
-            close(fd);
-        }
-    }
+    //int frk = fork();
+    // int fdw = open(nametowrite, O_WRONLY);
+    // int fdr = open(nametoread, O_RDONLY);
+    pthread_t thid, mythid, secthid, secthidid;
+    int result, secresult;
+    result = pthread_create( &thid, (pthread_attr_t *)NULL, mythread1, nametoread);
+    secresult = pthread_create( &secthid, (pthread_attr_t *)NULL, mythread2, nametowrite);
+    pthread_join(thid, (void **)NULL);
+    pthread_join(secthid, (void**)NULL);
 }
