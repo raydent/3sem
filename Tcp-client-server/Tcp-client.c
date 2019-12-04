16962d363a85e5cd9b95064be1f4fda8676cd2fb
Andrey Gavrilov <43818472+raydent@users.noreply.github.com>
2019-12-05 02:21:48 +0300

    almost working

--- Tcp-client-server/Tcp-client.c
+++ Tcp-client-server/Tcp-client.c
@@ -1,77 +1,30 @@
 /* Простой пример TCP-клиента для сервиса echo */
 #include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <string.h>
-#include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
-#include <stdlib.h>
-void main(int argc, char **argv)
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+
+void* Reading(void* socket) 
 {
-	int sockfd; 
-	/* Дескриптор сокета */
-	int n; 
-	/* Количество переданных или прочитанных символов */
-	int i; 
-	/* Счетчик цикла */
-	char sendline[1000],recvline[1000];
-	 /* Массивы для отсылаемой и принятой строки */
-	struct sockaddr_in servaddr; 
-	/* Структура для адреса сервера */
-	/* Сначала проверяем наличие второго аргумента вкомандной строке. При его отсутствии прекращаемработу */
-	if(argc != 2)
+	int* sockfd = (int*)socket;
+	int n;
+	while (1) 
 	{
-		printf("Usage: a.out <IP address>\n");
-		exit(1);
-	}
-	/* Обнуляем символьные массивы */
-	bzero(sendline,1000);
-	bzero(recvline,1000);
-	/* Создаем TCP-сокет */
-	if((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-	{
-		perror(NULL); 
-		/* Печатаем сообщение об ошибке */
-		exit(1);
-	}/* Заполняем структуру для адреса сервера: семействопротоколов TCP/IP, сетевой интерфейс – из аргументакомандной строки, номер порта 7. Поскольку в структуресодержится дополнительное не нужное нам поле, котороедолжно быть нулевым, перед заполнением обнуляем ее всю*/
-	bzero(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(51000);
-	if(inet_aton(argv[1], &servaddr.sin_addr) == 0)
-	{
-		printf("Invalid IP address\n");	
-		close(sockfd);
-		exit(1);
-	}/* Устанавливаем логическое соединение через созданный сокет с сокетом сервера, адрес которогомы занесли в структуру */
-	if(connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0)
-	{
-		perror(NULL);
-		close(sockfd);
-		exit(1);
-	}/* Три раза в цикле вводим строку с клавиатуры,отправляем ее серверу и читаем полученный ответ */
-//char* u_name = (char*) calloc (100, sizeof(char));
-//printf("Enter username:\n");
-//fgets(sendline, 1000, stdin);
-	if( (n = write(sockfd, sendline, strlen(sendline)+1)) < 0)
-	{
-		perror("Can\'t write\n");
-		close(sockfd);
-		exit(1);
-	}
-	for(i=0; i<3; i++)
-	{
-		printf("String => ");
-		fflush(stdin);
-		fgets(sendline, 1000, stdin);
-		if( (n = write(sockfd, sendline, strlen(sendline)+1)) < 0)
-		{
-			perror("Can\'t write\n");
-			close(sockfd);
-			exit(1);
-		}
-		if ( (n = read(sockfd,recvline, 999)) < 0)
+		char recvline[1000];
+		if ((n = read(sockfd, recvline, 999)) < 0)
 		{
 			perror("Can\'t read\n");
 			close(sockfd);
@@ -79,6 +32,61 @@ void main(int argc, char **argv)
 		}
 		printf("%s", recvline);
 	}
-	/* Завершаем соединение */
-	close(sockfd);
+}
+
+void* Writing(void* socket)
+	{
+	int* sockfd = (int*)socket;
+	int n;
+	while (1) 
+	{
+		char sendline[1000];
+		fflush(stdin);
+		fgets(sendline, 1000, stdin);
+		if ((n = write(sockfd, sendline, strlen(sendline) + 1)) < 0)
+		{
+			perror("Can\'t write\n");
+			close(sockfd);
+			exit(1);
+		}
+	}
+}
+
+int main(int argc, char** argv)
+{
+	pthread_t thid1, thid2;
+	int sockfd; 
+	char sendline[1000], recvline[1000]; 
+	struct sockaddr_in servaddr;
+	if (argc != 2) {
+		printf("Usage: a.out <IP address>\n");
+		exit(1);
+	}
+	bzero(sendline, 1000);
+	bzero(recvline, 1000);
+	if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
+		perror(NULL);
+		exit(1);
+	}
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(51000);
+	if (inet_aton(argv[1], &servaddr.sin_addr) == 0) {
+		printf("Invalid IP address\n");
+		close(sockfd);
+		exit(1);
+	}
+	if (connect(sockfd, (struct sockaddr*) & servaddr,
+		sizeof(servaddr)) < 0) {
+		perror(NULL);
+		close(sockfd);
+		exit(1);
+	}
+
+	pthread_create(&thid1, (pthread_attr_t*)NULL, Writing, sockfd);
+	pthread_create(&thid2, (pthread_attr_t*)NULL, Reading, sockfd);
+	pthread_join(thid1, (void**)NULL);
+	pthread_join(thid2, (void**)NULL);
+
+   close(sockfd);
 }
